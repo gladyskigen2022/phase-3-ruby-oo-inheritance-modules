7fb713c46bbd95c1cff2f2ffadef1e342b887c29
GladysKigen <gladys.kigen@student.moringa.com>
2023-02-16 17:23:38 +0300

    Completed

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,20 @@
-require_relative './dance_module'
-require_relative './meta_dancing_module'
+#require_relative './dance_module'
+#require_relative './meta_dancing_module'
+require_relative '../lib/fancy_dance.rb'
+
+#define our Dancer class and tell it to include the capabilities of the Dance module.
+
+class Dancer
+   #lend the Dancer class all of the methods of the Dance module using include keyword
+   extend FancyDance::ClassMethods
+   include FancyDance::InstanceMethods
+   #extend MetaDancing
+   #include Dance
+
+    attr_accessor :name
+    
+    def initialize(name)
+        @name = name
+    end
+end
+
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,18 @@
-require_relative './dance_module'
-require_relative './meta_dancing_module'
+#require_relative './dance_module'
+#require_relative './meta_dancing_module'
+require_relative '../lib/fancy_dance'
+#define our Kid class and tell it to include the capabilities of the Dance module.
+#lend the Kid class all of the methods of the Dance module using include keyword
+class Kid
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+    #extend MetaDancing
+    #include Dance
+
+    attr_accessor :name
+    
+    def initialize(name)
+        @name = name
+    end
+
+end
